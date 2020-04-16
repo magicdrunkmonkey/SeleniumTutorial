230f0ce99173539ee86855884c1941402898e8ab
George Sydby <george.sydby@gmail.com>
2020-04-16 14:15:00 +0200

    Pushar upp

---
+++ src/main/java/com/Selenium_for_intellij/Main.java
@@ -0,0 +1,42 @@
+package com.Selenium_for_intellij;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    WebDriver obj;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        //System.setProperty("webdriver.gecko.driver","C:\\Gecko\\geckodriver.exe"); //Firefox webdriver
+        System.setProperty("webdriver.chrome.driver","C:\\Dev_Drivers\\Chrome_Webdriver\\chromedriver.exe"); //Chrome webdriver
+        //System.setProperty("webdriver.edge.driver","C:\\Dev_Drivers\\MS_Edge_Webdriver\\msedgedriver.exe"); //Edge webdriver
+
+        WebDriver obj = new ChromeDriver();
+        obj.get("https://metrum-cloud-pilot.azurewebsites.net/WebForms/LogIn.aspx?ref=https://metrum-cloud-pilot.azurewebsites.net/WebForms/EventsPage.aspx");
+
+        Thread.sleep(2000);
+
+        //Fånga Metrum Logo på sidan Metrum PQX Web
+        WebElement webElement = obj.findElement(By.cssSelector("#metrumLogo"));
+        Screenshot screenshot = new AShot().takeScreenshot(obj, webElement);
+
+        Thread.sleep(2000);
+
+        ImageIO.write(screenshot.getImage(),"PNG",new File("C:\\Users\\george.sydby\\IdeaProjects\\Test_Selenium_Project\\Images\\MetrumLogo.PNG"));
+
+        Thread.sleep(2000);
+
+        obj.quit();
+
+    }
+}
