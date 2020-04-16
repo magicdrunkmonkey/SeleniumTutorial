1dbb047c7a4c7ff4e06206cfa1e3bb393bd4c716
George Sydby <george.sydby@gmail.com>
2020-04-16 13:07:57 +0200

    Pushar upp

---
+++ src/com/Selenium_for_intellij/ImageComparison.java
@@ -0,0 +1,10 @@
+package com.Selenium_for_intellij;
+import org.openqa.selenium.WebDriver;
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
+
+public class ImageComparison {
+
+    WebDriver obj;
+
+}
